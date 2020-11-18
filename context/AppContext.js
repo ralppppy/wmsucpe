04dd6b8f28b3d1dd65b7bd2e162897809bbf3684
ralppppy <ralpyosores@gmail.com>
2020-11-18 11:08:15 +0800

    Change proxy

--- context/AppContext.js
+++ context/AppContext.js
@@ -4,7 +4,7 @@ const AppContext = createContext();
 
 function AppProvider({ children }) {
   // const PROXY = "http://localhost:8080";
-  const PROXY = "https://wmsu-cpe-server.herokuapp.com";
+  const PROXY = "https://wmsu-cpe-admin.herokuapp.com";
 
   return (
     <AppContext.Provider value={{ proxy: PROXY }}>
